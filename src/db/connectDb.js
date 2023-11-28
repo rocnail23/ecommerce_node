5ff59e2276167803e8946cf86bc569b44975138a
rocnail23 <jdanielrojas16@gmail.com>
2023-11-27 23:13:06 -0400

    l

--- src/db/connectDb.js
+++ src/db/connectDb.js
@@ -1,6 +1,17 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.URI)
+const sequelize = new Sequelize('railway', 'postgres', '*gcGca34ee*GGGFFBabBbccd1fF-bE1g', {
+  dialect: 'postgres', // Reemplaza 'mysql' con el dialecto de tu base de datos
+  host: 'viaduct.proxy.rlwy.net',
+  port: 23138,
+  dialectOptions: {
+    // Opciones específicas del proxy si es necesario
+    // Ejemplo:
+    // ssl: true,
+    // rejectUnauthorized: false,
+  },
+});
+
 
 const connectDb = async () => {
   try {
